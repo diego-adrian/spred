392ffca7d348b17a2dd5514a09c7d72a54c0e501
Adrian <dbarra@agetic.gob.bo>
2019-10-02 16:05:51 -0400

    cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,12 @@ function mostrarMenu () {
 function ocultarMenu () {
   document.getElementById('menuLateral').classList.remove('mostrarMenuMobile');
 }
+
+$(document).scroll(function () {
+  var element = document.getElementById('menuNavegacion');
+  if (window.scrollY > 600) {
+    $('#menuNavegacion').addClass('backgroundBlue');
+  } else {
+    $('#menuNavegacion').removeClass('backgroundBlue');
+  }
+});
